f7c6f0db5f6124b3756a5b9d9a4ff2689ed8e854
pjechris <pjechris@users.noreply.github.com>
2022-08-19 13:48:56 +0200

    [#11] remove Path protocol

--- Sources/SimpleHTTP/Request/Endpoint.swift
+++ Sources/SimpleHTTP/Request/Endpoint.swift
@@ -1,18 +1,5 @@
 import Foundation
 
-/// A Type representing a URL path
-public protocol Path {
-  var path: String { get }
-}
-
-extension Path where Self: RawRepresentable, RawValue == String {
-  public var path: String { rawValue }
-}
-
-extension String: Path {
-  public var path: String { self }
-}
-
 /// A endpoint represents a path a request can query to
 ///
 /// You can create endpoints using plain String, for instance:
